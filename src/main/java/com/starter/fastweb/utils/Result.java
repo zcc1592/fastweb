95c70286c6e3a067bbf771e6af9cd92b9105610b
zhangchengcheng <zhangchengcheng@cn.wilmar-intl.com>
2021-03-10 20:45:49 +0800

    添加Result类

---
+++ src/main/java/com/starter/fastweb/utils/Result.java
@@ -0,0 +1,32 @@
+package com.starter.fastweb.utils;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 返回结果集
+ * @param <T>
+ */
+public class Result<T> implements Serializable {
+	private static final long serialVersionUID = 4760645896692860229L;
+	
+	private Integer code;
+	private String msg;
+	private boolean success;
+	private T data;
+	
+	public Result() {
+		super();
+	}
+	
+	public Result(Integer code, String msg, boolean success) {
+		super();
+		this.code = code;
+		this.msg = msg;
+		this.success = success;
+	}
+
+}
